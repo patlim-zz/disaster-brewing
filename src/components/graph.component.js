b331a43d0d34949cfb30587c59723302041a52d3
Pat-Lim <impatlim@gmail.com>
2020-09-17 22:25:41 +1200

    add offset areas based on setpoint

--- src/components/graph.component.js
+++ src/components/graph.component.js
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { AreaChart, Area, Line, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts"
 
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+]
 
 function Graph() {
   const [data, setData] = useState([])
@@ -13,30 +26,52 @@ function Graph() {
       .get("http://localhost:5000/temperatures")
       .then((res) => {
         if (res.data.length > 0) {
-          setData(res.data.map(dataset => {
-            const d = new Date(dataset.timestamp);
-            return {
-              timestamp: months[d.getMonth()],
-              temperature: dataset.temperature,
-              hot_switch: dataset.hot_switch,
-              cold_switch: dataset.cold_switch
-            }
-          }))
+          setData(
+            res.data.map((dataset) => {
+              const d = new Date(dataset.timestamp)
+              return {
+                timestamp: months[d.getMonth()],
+                temperature: dataset.temperature,
+                hot_switch: dataset.hot_switch,
+                cold_switch: dataset.cold_switch,
+              }
+            })
+          )
           setSetpoint(res.data[0].setpoint)
         }
       })
       .catch((err) => console.log(err))
-
   })
 
+  const gradientOffset = () => {
+    const dataMax = Math.max(...data.map((i) => i.temperature));
+    const dataMin = Math.min(...data.map((i) => i.temperature));
+  
+    return 1 - ((setpoint - dataMin) / (dataMax - dataMin))
+  }
+  
+  const off = gradientOffset();
+
   return (
-    <LineChart width={800} height={400} data={data.slice(100,300)}>
-      <Line type="monotone" dataKey="temperature" stroke="red"/>
-      <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
-      <XAxis dataKey="timestamp"/>
-      <YAxis domain={[18, 21]}/>
+    <AreaChart width={800} height={400} data={data}>
+      <Line type="monotone" dataKey="temperature" stroke="red" />
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="timestamp" />
+      <YAxis domain={[18, 21]} />
       <Tooltip />
-    </LineChart>
+      <defs>
+        <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
+            <stop offset={off} stopColor="green" stopOpacity={1}/>
+            <stop offset={off} stopColor="red" stopOpacity={1}/>
+          </linearGradient>
+      </defs>
+      <Area
+        type="monotone"
+        dataKey="temperature"
+        stroke="#000"
+        fill="url(#splitColor)"
+      />
+    </AreaChart>
   )
 }
 
