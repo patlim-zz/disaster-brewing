eb14ff79362036651363036b88c49e7278f5eded
Pat-Lim <impatlim@gmail.com>
2020-09-20 19:03:52 +1200

    style changes

--- src/components/graph.component.js
+++ src/components/graph.component.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
-import { AreaChart, Area, Line, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts"
+import {
+  AreaChart,
+  Area,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts"
 
 const months = [
   "January",
@@ -27,15 +35,17 @@ function Graph() {
       .then((res) => {
         if (res.data.length > 0) {
           setData(
-            res.data.map((dataset) => {
-              const d = new Date(dataset.timestamp)
-              return {
-                timestamp: months[d.getMonth()],
-                temperature: dataset.temperature,
-                hot_switch: dataset.hot_switch,
-                cold_switch: dataset.cold_switch,
-              }
-            })
+            res.data
+              .filter((e, i) => i % 10 === 10 - 1)
+              .map((dataset) => {
+                const d = new Date(dataset.timestamp)
+                return {
+                  timestamp: months[d.getMonth()],
+                  temperature: dataset.temperature,
+                  hot_switch: dataset.hot_switch,
+                  cold_switch: dataset.cold_switch,
+                }
+              })
           )
           setSetpoint(res.data[0].setpoint)
         }
@@ -43,14 +53,28 @@ function Graph() {
       .catch((err) => console.log(err))
   })
 
+  // const gradientOffset = () => {
+  //   const dataMax = Math.max(...data.map((i) => i.temperature))
+  //   const dataMin = Math.min(...data.map((i) => i.temperature))
+
+  //   if (dataMax <= 0) {
+  //     return 0
+  //   } else if (dataMin >= 0) {
+  //     return 1
+  //   } else {
+  //     return dataMax / (dataMax - dataMin)
+  //   }
+  // }
+  
   const gradientOffset = () => {
-    const dataMax = Math.max(...data.map((i) => i.temperature));
-    const dataMin = Math.min(...data.map((i) => i.temperature));
-  
-    return 1 - ((setpoint - dataMin) / (dataMax - dataMin))
+    const dataMax = Math.max(...data.map((i) => i.temperature))
+    const dataMin = Math.min(...data.map((i) => i.temperature))
+
+    return 1 - (setpoint - dataMin) / (dataMax - dataMin);
   }
-  
-  const off = gradientOffset();
+
+
+  const off = gradientOffset()
 
   return (
     <AreaChart width={800} height={400} data={data}>
@@ -61,14 +85,14 @@ function Graph() {
       <Tooltip />
       <defs>
         <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
-            <stop offset={off} stopColor="green" stopOpacity={1}/>
-            <stop offset={off} stopColor="red" stopOpacity={1}/>
-          </linearGradient>
+          <stop offset={off} stopColor="red" stopOpacity={1} />
+          <stop offset={off} stopColor="blue" stopOpacity={1} />
+        </linearGradient>
       </defs>
       <Area
         type="monotone"
         dataKey="temperature"
-        stroke="#000"
+        stroke="grey"
         fill="url(#splitColor)"
       />
     </AreaChart>
